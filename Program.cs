916d7271188329cdfb4b82aa4f9c02555d1e9b8a
keroxine <amb.sanchez14@gmail.com>
2023-07-07 12:17:04 -0600

    Types and Variables

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Compuerta Logica | Boolean 1 bit | true or false
+bool active = true;
+
+// Byte | 8 bits | 0 to 255
+byte angles = 100;
+
+//Character | 16 bits | All characters
+char initial = 'A';
+
+// Short | 16 bits | -32,768 to 32,767
+short length = 20000;
+
+// Integer | 32 bits | -2,147,483,648 to 2,147,483,647
+int year = 2023;
+
+// Float | 32 bits (Precision, F is mandatory) |±1.5e-45 to ±3.4e38, 7 digits precision
+float microDistance = 0.6458618F;
+
+// Double | 64 bits (La D indica que es un Double) |±5.0e-324 to ±1.7e308, 15-16 digits precision
+double grade = 99.99;
+
+// Long | 64 bits | -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 (No garantiza precision)
+long distance = 500000000000;
+
+//Decimal | 128 bits | ±1.0 x 10^-28 to ±7.9 x 10^28, 28 - 29 digits precision
+decimal measurement = 1.3543563465465465465465465M;
+
+// String | Dynamic Size
+string name = "Amber";
